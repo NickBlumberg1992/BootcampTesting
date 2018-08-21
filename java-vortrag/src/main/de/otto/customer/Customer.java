24df5dbdff040500aab125b5f0cb92b513367338
NickBlumberg1992 <42142954+NickBlumberg1992@users.noreply.github.com>
2018-08-21 10:23:14 +0200

    Add files via upload

---
+++ java-vortrag/src/main/de/otto/customer/Customer.java
@@ -0,0 +1,73 @@
+package de.otto.customer;
+
+import de.otto.article.Article;
+
+import java.util.*;
+
+public class Customer {
+    private static int customerCount = 0;
+    private String name;
+    private int age;
+    private Creditcard creditcard;
+    private Set<String> wishlist;
+
+    private Map<String, List<String>> boughtArticlesPerCategory;
+
+    public Customer(String name, int age, Creditcard creditcard) {
+        this.name = name;
+        this.age = age;
+        this.creditcard = creditcard;
+        this.wishlist = new HashSet<>();
+        this.boughtArticlesPerCategory = new HashMap<>();
+
+        customerCount++;
+    }
+
+    public static void resetCustomerCount() {
+        customerCount = 0;
+    }
+
+    public static int getCustomerCount() {
+        return customerCount;
+    }
+
+    public Map<String, List<String>> getBoughtArticlesPerCategory() {
+        return boughtArticlesPerCategory;
+    }
+
+    public int getWishlistSize() {
+        return wishlist.size();
+    }
+
+    public boolean addToWishlist(String articleNumber) {
+        if (wishlist.contains(articleNumber)) {
+            return false;
+        } else {
+            wishlist.add(articleNumber);
+            return true;
+        }
+    }
+
+    public boolean isFullAged() {
+        return age >= 18;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean buy(Article article) {
+        ArrayList<String> articles = new ArrayList<>();                     // ausgliedern? wird jedes mal neu instanziiert
+        articles.add(article.getNumber());
+        boughtArticlesPerCategory.put(article.getCategory(), articles);
+        return false;
+    }
+
+    public boolean isValid() {
+        if (isFullAged()) {
+            if (creditcard == null || creditcard.getNumber().isEmpty()) {
+                return false;
+            } else { return true; }}
+        else { return false; }
+    }
+}
